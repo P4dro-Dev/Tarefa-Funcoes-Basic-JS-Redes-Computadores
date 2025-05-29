ea2373c37e090fead9db8d4414177229762cbb5a
Pedro Henrique <Ph9084533@gmail.com>
2025-05-29 18:27:46 -0300

    Add files via upload
    Adicionando os script's em JavaScript com às questões respondidas. E os enunciados comentados!

---
+++ Funções-JS/VI.js
@@ -0,0 +1,8 @@
+/**
+ * Atualiza a idade de um objeto pessoa
+ * @param {object} pessoa - Objeto com propriedades nome e idade
+ * @param {number} novaIdade - Nova idade para atualizar
+ */
+function atualizarIdade(pessoa, novaIdade) {
+  pessoa.idade = novaIdade;
+}
